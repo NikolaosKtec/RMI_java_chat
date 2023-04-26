e82474259ee835250827ed1ae003a6be03040171
Nikolaos Ferreira Daskalakis <87676644+NikolaosKtec@users.noreply.github.com>
2023-04-26 10:56:37 -0300

    Update Client_chat.java

--- client/Client_chat.java
+++ client/Client_chat.java
@@ -2,6 +2,7 @@ package client;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 // import java.time.LocalDate;
+import java.util.Scanner;
 
 import chat.Chat_interface;
 // import data.User;
@@ -12,8 +13,11 @@ public class Client_chat {
     private Client_chat() {}
 
     public static void main(String[] args) {
-
-        String host = (args.length < 1) ? null : args[0];
+         String host = (args.length < 1) ? null : args[0];
+         Scanner myObj = new Scanner(System.in);
+         String userName = "";
+         String message = "";
+       
         try {
             Registry registry = LocateRegistry.getRegistry(host);
             Chat_interface obj_remote = (Chat_interface) registry.lookup("rmi://localhost/SERVER");
@@ -22,10 +26,28 @@ public class Client_chat {
             // User u = new User("niko", "Af0!wqFas2023", LocalDate.now(), true);
                 //todo algum problema de serialização
             // String response = obj_remote.join_chat("nikof1", u);
-
-            String response =  obj_remote.get_date_time("teste");
+            
+            System.out.println("entrando no chat digite x para sair;");
+            System.out.println("escreva seu nome de usuário abaixo:");
+            userName = myObj.nextLine(); 
+            
            
-            System.out.println("response: " + response);
+           
+            while(true){
+                // input
+                System.out.println("envie sua mensagem abaixo:");
+                message = myObj.nextLine();
+                
+                if(message.equals("x")|| message.equals("X")){
+                    break;
+                }
+                String response =  obj_remote.send_message(message,userName);
+           
+                System.out.println("chat: " + response);
+
+            }
+
+            
 
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
@@ -35,15 +57,4 @@ public class Client_chat {
 
 
 
-
-    // public static void main(String[] args) {
-    //     try {
-    //         Registry registry = LocateRegistry.getRegistry();
-    //         Hello_interface objRemoto = (Hello_interface) registry.lookup("rmi://localhost/SERVIDOR");
-    //         String mensagem = "ola mundo";
-    //         System.out.println("Nova frase : "+ objRemoto.sayHello(mensagem));
-    //     } catch (Exception e) {
-    //         System.out.println("Erro no cliente:"+ e);
-    //     }
-    // }
 }
